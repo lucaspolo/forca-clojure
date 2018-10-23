c3c5860ca390313f6ff5b71b8f555689f156a106
Lucas Polo <pradopolo@gmail.com>
2018-10-22 22:52:14 -0300

    First commit

---
+++ src/forca/core.clj
@@ -0,0 +1,40 @@
+(ns forca.core
+  (:gen-class))
+
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu!\n"))
+(defn ganhou [] (print "Você ganhou!\n"))
+
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra)
+)
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos))
+)
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn le-letra! [] (read-line))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)
+  )
+)
+
+(defn jogo [vidas palavra acertos]
+  (if (= vidas 0) 
+    (perdeu)
+    (if (acertou-a-palavra-toda? palavra acertos)
+      (ganhou)
+      (avalia-chute (le-letra!) vidas palavra acertos)
+    )
+  )
+)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
