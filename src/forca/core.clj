698af28f6e3216d771239994734f57cb973eaea3
Lucas Polo <pradopolo@gmail.com>
2018-10-22 23:17:53 -0300

    Alterado para jogo usar recursao em cauda
    close #1

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -7,32 +7,29 @@
 (defn ganhou [] (print "Você ganhou!\n"))
 
 (defn letras-faltantes [palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra)
-)
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
 (defn acertou-a-palavra-toda? [palavra acertos]
-  (empty? (letras-faltantes palavra acertos))
-)
+  (empty? (letras-faltantes palavra acertos)))
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
 (defn le-letra! [] (read-line))
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)
-  )
-)
-
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0) 
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos)
-    )
-  )
-)
+  (cond 
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+    :else
+    (let [chute (le-letra!)]
+      (if (acertou? chute palavra)
+        (do
+          (println "Acertou a letra!") 
+          (recur vidas palavra (conj acertos chute))
+        )
+        (do
+          (println "Errou a letra!")
+          (recur (dec vidas) palavra acertos))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
